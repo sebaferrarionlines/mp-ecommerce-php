ca50cf2d83f593fa8c99a450fe711bb3c9912313
sebastianferrari_onlines <sebastian.ferrari@onlines.com.ar>
2020-10-04 20:51:57 -0300

    Testing notifications 2

--- webhook.php
+++ webhook.php
@@ -1,25 +1,27 @@
 <?php
-require_once 'vendor/autoload.php';
-MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-
-header("HTTP/1.1 200 OK");
-
-switch($_POST["type"]) {
-    case "payment":
-        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-        print_r($payment);
-        break;
-    case "plan":
-        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-        print_r($plan);
-        break;
-    case "subscription":
-        $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-        print_r($plan);
-        break;
-    case "invoice":
-        $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-        print_r($plan);
-        break;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $myfile = fopen("info1.txt", "a") or die("Unable to open file!");
+    
+    $txt = file_get_contents('php://input');
+    fwrite($myfile, $txt);
+    
+    fclose($myfile);
+    
+    $myfile2 = fopen("info2.txt", "w") or die("Unable to open file!");
+    $txt2 = "Entro al POST.";
+    fwrite($myfile2, $txt2);
+    fclose($myfile2);
 }
-?>
+else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $myfile = fopen("info2.txt", "a") or die("Unable to open file!");
+    $txt = json_encode($_GET);
+    fwrite($myfile, $txt);
+    fclose($myfile);
+}
+
+$myfile = fopen("info.txt", "a") or die("Unable to open file!");
+$txt = json_encode($_REQUEST);
+fwrite($myfile, $txt);
+fclose($myfile);
+
+http_response_code(200);
