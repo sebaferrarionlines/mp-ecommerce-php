8e78296bb2175a7175b1604a389954a0914b8c61
sebastianferrari_onlines <sebastian.ferrari@onlines.com.ar>
2020-10-04 20:45:42 -0300

    Testing notifications

--- webhook.php
+++ webhook.php
@@ -1,32 +1,25 @@
 <?php
-$json = file_get_contents('php://input');
-// Converts it into a PHP object
-
-$logFile = fopen("log-mp-json.txt", 'a') or die("Error creando archivo");
-fwrite($logFile, print_r($json, true));
-fclose($logFile);
-
 require_once 'vendor/autoload.php';
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-
-if ($_GET["topic"] == "payment") {
-        
-    $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
-    $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
-
-    //guardo en un archivo payment        
-    $logFile = fopen("log-mp-payment.txt", 'a') or die("Error creando archivo");
-    fwrite($logFile, print_r($payment, true));
-    fclose($logFile);
-
-    //guardo en un archivo merchant_order
-    $logFile = fopen("log-mp-merchant.txt", 'a') or die("Error creando archivo");
-    fwrite($logFile, print_r($merchant_order, true));
-    fclose($logFile);
-}
-
-   
-ob_clean();
 header("HTTP/1.1 200 OK");
+
+switch($_POST["type"]) {
+    case "payment":
+        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        print_r($payment);
+        break;
+    case "plan":
+        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+        print_r($plan);
+        break;
+    case "subscription":
+        $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+        print_r($plan);
+        break;
+    case "invoice":
+        $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+        print_r($plan);
+        break;
+}
 ?>
